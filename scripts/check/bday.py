6fe05cfb791f91ed166a60786270b7f6b626c418
Vanadiry <77710715+Vanadiry@users.noreply.github.com>
2026-08-30 18:06:22 +0800

    fix(scripts): bday调用

--- scripts/check/bday.py
+++ scripts/check/bday.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import yaml
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "lib"))
 sys.path.insert(0, str(Path(__file__).resolve().parent))
 from lib import bangumi, moegirl
 from lib.status import status_mark
